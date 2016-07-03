4636b117ceaf721dbdfa6a98882389e97646d0ce
Ziyi Bao <ziyi.bao14@imperial.ac.uk>
2016-07-03 21:59:53 +0100

    added classes to represent aba framework and method for basic WCP checking

--- aba_plus.py
+++ aba_plus.py
@@ -1,6 +1,134 @@
 import numpy as np
 
+LESS_THAN = 1
+LESS_EQUAL = 2
 NO_RELATION = 3
+
+class ABA_Plus:
+    def __init__(self, assumptions=set(), preferences=set(), rules=set()):
+        self.assumptions = assumptions
+        self.preferences = preferences
+        self.rules = rules
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def attacking_rules(self, assumption):
+        return self.deriving_rules(assumption.negated())
+
+    def deriving_rules(self, assumption):
+        der_rules = set()
+        for rule in self.rules:
+            if rule.consequent == assumption:
+                der_rules.add(rule)
+        return der_rules
+
+    def get_relation(self, assump1, assump2):
+        for pref in self.preferences:
+            if pref.assump1 == assump1 and pref.assump2 == assump2:
+                return pref.relation
+        return NO_RELATION
+
+    def is_preferred(self, assump1, assump2):
+        return self.get_relation(assump2, assump1) == LESS_THAN
+
+    def WCP_fulfilled(self, contradictor, assumption, antecedent):
+        negated_contr = contradictor.negated()
+        deduce_from = antecedent.copy()
+        deduce_from.add(assumption)
+        deduce_from.remove(contradictor)
+        return self.deduction_exists(negated_contr, deduce_from)
+
+    def deduction_exists(self, to_deduce, deduce_from):
+        rules_applied = set()
+        deduced = deduce_from.copy()
+        while True:
+            new_rule_used = False
+            for rule in self.rules:
+                if rule not in rules_applied:
+                    if rule.antecedent.issubset(deduced):
+                        new_rule_used = True
+                        if rule.consequent == to_deduce:
+                            return True
+                        else:
+                            deduced.add(rule.consequent)
+                        rules_applied.add(rule)
+            if not new_rule_used:
+                break
+        return False
+
+    def direct_decution_exists(self, to_deduce, deduce_from):
+        for rule in self.rules:
+            if rule.consequent == to_deduce and rule.antecedent.issubset(deduce_from):
+                return True
+        return False
+
+    def check_WCP(self):
+        for assump in self.assumptions:
+            att_rules = self.attacking_rules(assump)
+            for rule in att_rules:
+                for contradictor in rule.antecedent:
+                    if not self.preference_check(assump, contradictor, rule.antecedent):
+                        return False
+        return True
+
+    def preference_check(self, assumption, contradictor, antecedent):
+        if self.is_preferred(assumption, contradictor) and \
+           not self.WCP_fulfilled(contradictor, assumption, antecedent):
+            return False
+        elif contradictor not in self.assumptions:
+            der_rules = self.deriving_rules(contradictor)
+            for rule in der_rules:
+                for ant in rule.antecedent:
+                    if not self.preference_check(assumption, ant, rule.antecedent):
+                        return False
+        return True
+
+
+class Rule:
+    def __init__(self, antecedent=None, consequent=None):
+        self.antecedent = antecedent
+        self.consequent = consequent
+
+    def __eq__(self, other):
+        return self.antecedent == other.antecedent and \
+               self.consequent == other.consequent
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __hash__(self):
+        return (tuple(self.antecedent), self.consequent).__hash__()
+
+class Predicate:
+    def __init__(self, symbol=None, is_negated=False):
+        self.symbol = symbol
+        self.is_negated = is_negated
+
+    def __eq__(self, other):
+        return self.is_negated == other.is_negated and \
+               self.symbol == other.symbol
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __hash__(self):
+        return (self.symbol, self.is_negated).__hash__()
+
+    def negated(self):
+        return Predicate(self.symbol, not self.is_negated)
+
+class Preference:
+    def __init__(self, assump1=None, assump2=None, relation=NO_RELATION):
+        self.assump1 = assump1
+        self.assump2 = assump2
+        self.relation = relation
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __hash__(self):
+        return (self.assump1, self.assump2, self.relation).__hash__()
 
 # in relation_matrix, use 1 for < and 2 for <=
 def transitive_closure(relation_matrix):
---
+++ test.py
@@ -0,0 +1,153 @@
+import unittest
+from aba_plus import *
+
+class TestABAPlus(unittest.TestCase):
+
+    def test_simple_deduction_exists(self):
+        a = Predicate("a")
+        b = Predicate("b")
+        assumptions = set([b])
+
+        rule = Rule(set([b]),a)
+        rules = (set([rule]))
+
+        abap = ABA_Plus(assumptions=assumptions, rules=rules)
+
+        self.assertTrue(abap.deduction_exists(to_deduce=a, deduce_from=set([b])))
+
+    def test_simple_deduction_does_not_exist(self):
+        a = Predicate("a")
+        b = Predicate("b")
+        c = Predicate("c")
+        assumptions = set([a, b])
+
+        rule = Rule(set([b]), c)
+        rules = (set([rule]))
+
+        abap = ABA_Plus(assumptions=assumptions, rules=rules)
+
+        self.assertFalse(abap.deduction_exists(to_deduce=a, deduce_from=set([b])))
+
+    def test_transitive_deduction_exists(self):
+        a = Predicate("a")
+        b = Predicate("b")
+        c = Predicate("c")
+        assumptions = set([b])
+
+        rule1 = Rule(set([b]), c)
+        rule2 = Rule(set([c]), a)
+        rules = (set([rule1, rule2]))
+
+        abap = ABA_Plus(assumptions=assumptions, rules=rules)
+
+        self.assertTrue(abap.deduction_exists(to_deduce=a, deduce_from=set([b])))
+
+    def test_complex_deduction_exists(self):
+        a = Predicate("a")
+        b = Predicate("b")
+        c = Predicate("c")
+        d = Predicate("d")
+        e = Predicate("e")
+        f = Predicate("f")
+        g = Predicate("g")
+        assumptions = set([a, b, e])
+
+        rule1 = Rule(set([a, b]), c)
+        rule2 = Rule(set([e]), f)
+        rule3 = Rule(set([c, f]), g)
+        rules = (set([rule1, rule2, rule3]))
+
+        abap = ABA_Plus(assumptions=assumptions, rules=rules)
+
+        self.assertTrue(abap.deduction_exists(to_deduce=g, deduce_from=set([a, b, e])))
+
+    def test_complex_deduction_does_not_exist(self):
+        a = Predicate("a")
+        b = Predicate("b")
+        c = Predicate("c")
+        d = Predicate("d")
+        e = Predicate("e")
+        f = Predicate("f")
+        g = Predicate("g")
+        assumptions = set([a, b, e])
+
+        rule1 = Rule(set([a, b]), c)
+        rule2 = Rule(set([e]), f)
+        rule3 = Rule(set([c, f]), g)
+        rules = (set([rule1, rule2, rule3]))
+
+        abap = ABA_Plus(assumptions=assumptions, rules=rules)
+
+        self.assertFalse(abap.deduction_exists(to_deduce=g, deduce_from=set([a, e])))
+
+    def test_simple_WCP_no_violation_check1(self):
+        a = Predicate("a")
+        b = Predicate("b")
+        c = Predicate("c")
+        assumptions = set([a, b])
+
+        rule = Rule(set([b]), c)
+        rules = (set([rule]))
+
+        abap = ABA_Plus(assumptions=assumptions, rules=rules)
+
+        self.assertTrue(abap.check_WCP())
+
+    def test_simple_WCP_no_violation_check2(self):
+        a = Predicate("a")
+        b = Predicate("b")
+        assumptions = set([a, b])
+
+        rule1 = Rule(set([b]), a.negated())
+        rule2 = Rule(set([a]), b.negated())
+        rules = (set([rule1, rule2]))
+
+        abap = ABA_Plus(assumptions=assumptions, rules=rules)
+
+        self.assertTrue(abap.check_WCP())
+
+    def test_simple_WCP_no_violation_check3(self):
+        a = Predicate("a")
+        b = Predicate("b")
+        c = Predicate("c")
+        assumptions = set([a, b, c])
+
+        rule1 = Rule(set([b]), a.negated())
+        rule2 = Rule(set([c]), b.negated())
+        rules = (set([rule1, rule2]))
+
+        abap = ABA_Plus(assumptions=assumptions, rules=rules)
+
+        self.assertTrue(abap.check_WCP())
+
+    def test_simple_WCP_violation_check(self):
+        a = Predicate("a")
+        b = Predicate("b")
+        assumptions = set([a, b])
+
+        pref = Preference(b, a, LESS_THAN)
+        preferences = set([pref])
+
+        rule = Rule(set([b]), a.negated())
+        rules = (set([rule]))
+
+        abap = ABA_Plus(assumptions=assumptions, preferences=preferences, rules=rules)
+
+        self.assertFalse(abap.check_WCP())
+
+    def test_transitive_WCP_violation_check(self):
+        a = Predicate("a")
+        b = Predicate("b")
+        c = Predicate("c")
+        assumptions = set([a, b])
+
+        pref = Preference(b, a, LESS_THAN)
+        preferences = set([pref])
+
+        rule1 = Rule(set([b]), c)
+        rule2 = Rule(set([c]), a.negated())
+        rules = (set([rule1, rule2]))
+
+        abap = ABA_Plus(assumptions=assumptions, preferences=preferences, rules=rules)
+
+        self.assertFalse(abap.check_WCP())
