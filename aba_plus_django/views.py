00d4220e9b0bf2c4cbcfff9f9dadb8564fb12518
Ziyi Bao <ziyi.bao14@imperial.ac.uk>
2016-07-29 18:31:34 +0100

    homepage now uses new text and new template

--- aba_plus_django/views.py
+++ aba_plus_django/views.py
@@ -1,7 +1,16 @@
 from django.views import generic
+from django.http import HttpResponseRedirect
 
 class IndexView(generic.ListView):
     template_name = 'aba_plus_django/index.html'
 
     def get_queryset(self):
         return None
+
+    def post(self, request):
+        file = request.FILES['myfile']
+        str = ""
+        for chunk in file.chunks():
+            str += chunk.decode("utf-8")
+        print(str)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
