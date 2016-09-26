01bb12d186b74250717445e3a1a61b281f70ea9d
Ziyi Bao <ziyi.bao14@imperial.ac.uk>
2016-09-26 17:09:36 +0200

    Added error pages with home link in case of exception

--- aba_plus_django/middleware/exception_handler.py
+++ aba_plus_django/middleware/exception_handler.py
@@ -3,7 +3,6 @@ __email__ = "zb714@ic.ac.uk"
 __copyright__ = "Copyright (c) 2016 Ziyi Bao"
 
 from abap_parser import *
-from django.http import HttpResponse
 from django.shortcuts import render
 
 class ExceptionMiddleware(object):
@@ -14,9 +13,13 @@ class ExceptionMiddleware(object):
            isinstance(exception, DuplicateSymbolException) or \
            isinstance(exception, InvalidContraryDeclarationException) or \
            isinstance(exception, InvalidPreferenceDeclarationException):
-            return HttpResponse(exception.message)
+            return render(request, template_name='../templates/aba_plus_django/error_page.html',
+                          context={'msg':exception.message})
         elif isinstance(exception, WCPViolationException):
             return render(request, template_name='../templates/aba_plus_django/auto_wcp.html')
         elif isinstance(exception, KeyError):
-            return HttpResponse("Session has ended.")
+            return render(request, template_name='../templates/aba_plus_django/error_page.html',
+                          context={'msg': "Session has ended."})
+        #else:
+           # return HttpResponse("An unknown error has occured.")
         return None
